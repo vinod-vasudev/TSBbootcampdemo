ad5395c2d0e2350b15adb7d857d09128387f46da
ravicollabera <150028025+ravicollabera@users.noreply.github.com>
2024-01-12 00:19:07 +0530

    Delete accounts directory

--- accounts/src/main/java/com/ob/tsb/accounts/exception/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.ob.tsb.accounts.exception;
-
-import org.springframework.http.HttpStatusCode;
-
-public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException(HttpStatusCode httpStatusCode, String message) {
-        super(message);
-    }
-}
