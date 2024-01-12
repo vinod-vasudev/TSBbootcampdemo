dc834d2fc0cd797287af9779ef92ba910b3f366d
ravicollabera <150028025+ravicollabera@users.noreply.github.com>
2024-01-12 20:31:33 +0530

    Delete balances directory

--- balances/src/main/java/com/ob/tsb/balances/exception/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.ob.tsb.balances.exception;
-
-import org.springframework.http.HttpStatusCode;
-
-public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException(HttpStatusCode httpStatusCode, String message) {
-        super(message);
-    }
-}
--- balances/src/main/java/com/ob/tsb/balances/exception/UnauthorizedException.java
+++
@@ -1,9 +0,0 @@
-package com.ob.tsb.balances.exception;
-
-import org.springframework.http.HttpStatusCode;
-
-public class UnauthorizedException extends RuntimeException{
-    public UnauthorizedException(HttpStatusCode httpStatusCode, String message) {
-        super(message);
-    }
-}
