5ff13fe6939bc5010a807a30732225210c97e39c
ravicollabera <150028025+ravicollabera@users.noreply.github.com>
2024-01-14 20:58:09 +0530

    Delete accountsDetails directory

--- accountsDetails/src/main/java/com/tsb/accountsDetails/controller/AccountDetailsApiVersion.java
+++
@@ -1,7 +0,0 @@
-package com.tsb.accountsDetails.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@RequestMapping("/api/accountDetails/v1")
-public interface AccountDetailsApiVersion {
-}
